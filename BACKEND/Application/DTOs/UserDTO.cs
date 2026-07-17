2d1fc865f27a8c889890a76f01e891c173e3d609
vivek9579 <viveksrivastav933@gmail.com>
2026-07-17 19:25:18 +0530

    Removed Unused File

--- BACKEND/Application/DTOs/UserDTO.cs
+++ BACKEND/Application/DTOs/UserDTO.cs
@@ -6,9 +6,9 @@ namespace Application.DTOs
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
+        public string? Role { get; set; }
     }
 }
